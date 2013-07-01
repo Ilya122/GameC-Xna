40b86cd55ef8cde77bc40983ac38a425a2510320
Ilya122 <Sorcerer122@gmail.com>
2013-07-01 17:32:52 +0300

    Update ProgressBar.cs

--- ProgressBar.cs
+++ ProgressBar.cs
@@ -10,19 +10,21 @@ namespace GameClasses
     [Serializable]
     public class ProgressBar
     {
-        Sprite picture;             //Static Tex.
+        Texture2D picture;             //Static Tex.
         Texture2D redTexture;       //Tex which moves.
         Vector2 position;
         int Value;
         int MaxValue;
         Color color = Color.Red;
-        float scaleX;
-        float scaleY;
+        float scaleX = 1.0f;
+        float scaleY = 1.0f;
 
+        private Rectangle PictureRec
+        { get { return new Rectangle((int)this.position.X, (int)this.position.Y, (int)(picture.Width*this.scaleX), (int)(picture.Height*this.scaleY)); } }
         //Constructors:
         public ProgressBar(Texture2D tex, Texture2D secondTex, Vector2 pos, int value, Color c, int scaleX, int scaleY)
         {
-            picture = new Sprite(tex, pos);
+            picture = tex;
             this.position = pos;
             this.redTexture = secondTex;
             this.Value = value;
@@ -35,7 +37,7 @@ namespace GameClasses
             Vector2 pos, int width,int height, int value, int maxvalue)
         {
             TexRectanlge r = new TexRectanlge(gd, secondcolor, width, height, (int)pos.X, (int)pos.Y);
-            this.picture = new Sprite(r.Texture, pos);
+            this.picture = r.Texture;
             color = firstcolor;
             TexRectanlge r2 = new TexRectanlge(gd, firstcolor, width, height, (int)pos.X, (int)pos.Y);
             this.redTexture = r2.Texture;
@@ -52,17 +54,16 @@ namespace GameClasses
             this.Value = (int)MathHelper.Clamp(this.Value, 0, this.MaxValue);
         }
 
-        public void Draw(SpriteBatch sb, Game g)
+        public void Draw(SpriteBatch sb)
         {
-            picture.Draw(sb,this.scaleX,this.scaleY);
+            sb.Draw(picture, PictureRec, Color.White);
             sb.Draw(redTexture, new Rectangle((int)position.X, (int)position.Y
 , (int)MathHelper.Clamp((int)(this.scaleX * redTexture.Width * ((double)Value / this.MaxValue)),0,this.scaleX*redTexture.Width)
-                , (int)(picture.Tex.Height * this.scaleY)), this.color);
+                , (int)(picture.Height * this.scaleY)), this.color);
 
         }
 
         //Properties:
-        public Sprite Picture { get { return this.picture; } set { this.picture = value; } }
         public Vector2 Position { get { return this.position; } set { this.position = value; } }
         public float ScaleX { get { return this.scaleX; } set {  this.scaleX = value; } }
         public float ScaleY { get { return this.scaleY; } set { this.scaleY = value; } }
